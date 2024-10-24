56b62d5cdf07a7516076679fc248b18a76bd5b24
Bertodo Neto <100353344+Bertodo015@users.noreply.github.com>
2024-10-23 20:39:48 -0400

    commit 23-10
    Implementação dos testes de erro para cada instância

--- src/models/Contact.test.ts
+++ src/models/Contact.test.ts
@@ -26,19 +26,62 @@ describe("Tests over Contact class", () => {
 
     it("should not create a contact with invalid name", () => {
         const nameValue = "Tal";
-        const phoneValue = "(99) 99999-9999";
-        const emailValue = "fulano@email.com";
 
         try {
             const contact = new Contact();
             contact.name = nameValue;
-            contact.phone = phoneValue;
-            contact.email = emailValue;
+            fail("It allowed to set an invalid name");
         } catch (erro) {
             expect(erro).toBeInstanceOf(ContactCreationError);
             expect(erro.message).toBe(ContactErrorMessages.NAME_ERROR_MESSAGE);
         }
+    });
 
-        
+    it("should not create a contact with invalid phone", () => {
+        const contact = new Contact();
+
+        try {
+            contact.phone = "12345-6789";
+            fail("It allowed to set an invalid phone");
+        } catch(erro) {
+            expect(erro).toBeInstanceOf(ContactCreationError);
+            expect(erro.message).toBe(ContactErrorMessages.PHONE_ERROR_MESSAGE);
+        }
+    });
+
+    it("should not create a contact with invalid email", () => {
+        const contact = new Contact();
+
+        try {
+            contact.email = "fulano@";
+            fail("It allowed to set an invalid email");
+        } catch(erro) {
+            expect(erro).toBeInstanceOf(ContactCreationError);
+            expect(erro.message).toBe(ContactErrorMessages.EMAIL_ERROR_MESSAGE);
+        }
+    });
+
+    it("shold not create a contact with invalid address", () => {
+        const contact = new Contact();
+
+        try {
+            contact.address = "Rua";
+            fail("It allowed to set an invalid address");
+        } catch(erro) {
+            expect(erro).toBeInstanceOf(ContactCreationError);
+            expect(erro.message).toBe(ContactErrorMessages.ADDRESS_ERROR_MESSAGE);
+        }
+    });
+
+    it("should not create a contact with invalid birthday", () => {
+        const contact = new Contact();
+
+        try {
+            contact.birthday = new Date("2031-02-30");
+            fail("It allowed to set an invalid birthday");
+        } catch(erro) {
+            expect(erro).toBeInstanceOf(ContactCreationError);
+            expect(erro.message).toBe(ContactErrorMessages.BIRTHDAY_ERROR_MESSAGE);
+        }
     });
 });
